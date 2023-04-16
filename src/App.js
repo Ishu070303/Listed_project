0302df127f857a3df545be86b6d8e2a3464744c5
Eshika rawat <eshikarawat02@gmail.com>
2023-04-17 00:28:26 +0530

    added comments

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import './style.css';
-
 import Login from './components/Login/Login';
 
 const App = () => {
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,34 +5,42 @@ import './login.css';
 const Login = () => {
   return (
     <>
+    {/*Left Banner */}
     <div className='left-side'>
         <h1>Board.</h1>
     </div>
+
+    {/*Right Banner */}
     <div className='right-side'>
+
+        {/*LOGIN Page */}
         <div className='right-div'>
         <h2>Sign In</h2>
         <h5 className='t1'>Sign in to your account</h5>
+
+        {/*Google button */}
         <button className='google-button'><i class="ri-google-fill" style={{marginRight: '12px'}} ></i>Sign in with Google</button>
+        {/*Apple Button */}
         <button className='apple-button'><i class="ri-apple-fill" style={{marginRight: '12px'}} ></i>Sign in with Apple</button>
+        
         <div className='div-under'>
+            {/*FORM Starting*/}
             <form >
                 <label className='l1'>Email Address</label><br />
                 <input className='i1' type='text' /><br />
                 <label className='l1'>Password</label><br />
                 <input className='i1' type='password'/>
                 <h5 className='link' ><a>Forgot Password?</a></h5>
-
                 <button className='submit-button'>SIGN IN</button>
             </form>
         </div>
-
-        <h5 className='t2'>Don't have an account ? <a className='link-register'>Register</a></h5>
+        <h5 className='t2'>Don't have an account? <a className='link-register'>Register</a></h5>
         </div>
-
+        {/*FORM Ending*/}
     </div>
     
     </>
   )
 }
 
-export default Login
+export default Login;
