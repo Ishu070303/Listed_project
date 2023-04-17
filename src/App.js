55f42a52fc948b517b19a3023adb6ba8f9a3acea
Eshika rawat <eshikarawat02@gmail.com>
2023-04-17 09:16:34 +0530

    added more styling

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import './style.css';
 import Login from './components/Login/Login';
+import Dashboard from './components/Dashboard/Dashboard';
 
 const App = () => {
   return (
     <>
-    <Login />
+      {/* <Dashboard /> */}
+      <Login />
     </>
   )
 }
---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import SideBar from './SideBar'
+
+const Dashboard = () => {
+  return (
+    <div className='dash'>
+    <SideBar />
+    
+    </div>
+  )
+}
+
+export default Dashboard
