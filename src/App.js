59dda7927e0b8a1fbeff002dcdfd12c14c62ff4f
Eshika rawat <eshikarawat02@gmail.com>
2023-04-19 20:28:36 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 
 import './style.css';
 import Login from './components/Login/Login';
-// import Dashboard from './components/Dashboard/Dashboard';
-// import { Route, Routes } from 'react-router-dom';
+import Dashboard from './components/Dashboard/Dashboard';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
-     <Login />
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/login' element={<Login />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+    </Routes>
+    
+    </BrowserRouter>
   )
 }
 
